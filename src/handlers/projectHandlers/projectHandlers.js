7ce72874f21895f33e9b91bfb5d25266676f6f19
Martín <martingaliottimartinez@gmail.com>
2024-02-18 16:35:18 -0300

    creacion de proyectos

--- src/handlers/projectHandlers/projectHandlers.js
+++ src/handlers/projectHandlers/projectHandlers.js
@@ -18,14 +18,35 @@ const getProjectsHandler = async (req, res) => {
 
 const createProjectsHandler = async (req, res) => {
   const { id } = req.params;
+  const {
+    title,
+    description,
+    url,
+    image,
+    startDate,
+    endDate,
+    skills,
+    technologies,
+  } = req.body;
   try {
-    const response = await createProjectsController(id);
+    const response = await createProjectsController(
+      id,
+      title,
+      description,
+      url,
+      image,
+      startDate,
+      endDate,
+      skills,
+      technologies
+    );
     if (!response || response.error) {
       res.status(400).json(response);
     } else {
       res.status(201).json(response);
     }
   } catch (error) {
+    console.log(error);
     res.status(400).json({ error: true, message: error.message });
   }
 };
