8bb87d5f0f939383bf45bd9026b530c4cb6acc5b
Martín <martingaliottimartinez@gmail.com>
2024-02-18 20:37:29 -0300

    Eliminando los Timestamps

--- src/models/User/Credentials.js
+++ src/models/User/Credentials.js
@@ -1,22 +1,28 @@
 const { DataTypes } = require("sequelize");
 
 module.exports = (sequelize) => {
-  sequelize.define("Credentials", {
-    id: {
-      type: DataTypes.UUID,
-      primaryKey: true,
-      allowNull: false,
-      unique: true,
-      defaultValue: DataTypes.UUIDV4,
+  sequelize.define(
+    "Credentials",
+    {
+      id: {
+        type: DataTypes.UUID,
+        primaryKey: true,
+        allowNull: false,
+        unique: true,
+        defaultValue: DataTypes.UUIDV4,
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
     },
-    email: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      unique: true,
-    },
-    password: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-  });
+    {
+      timestamps: false,
+    }
+  );
 };
